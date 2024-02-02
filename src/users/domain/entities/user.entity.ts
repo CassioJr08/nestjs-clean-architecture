eeb90cfa26ec6e55ab30ae98d5c4ccd7fcb78d09
Cassio <cassiojr0108@gmail.com>
2024-02-02 19:02:33 -0300

    Adding class UserValidator and UserValidatorFactory

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,4 +1,5 @@
 import { Entity } from '@/shared/domain/entities/entity'
+import { UserValidatorFactory } from '../validators/user.validator'
 export type UserProps = {
   name: string
   email: string
@@ -7,15 +8,24 @@ export type UserProps = {
 }
 export class UserEntity extends Entity<UserProps> {
   constructor(public readonly props: UserProps, id?: string) {
+    UserEntity.validate(props)
     super(props, id)
     this.props.createdAt = this.props.createdAt ?? new Date()
   }
 
   update(value: string): void {
+    UserEntity.validate({
+        ...this.props,
+        name: value,
+      })
     this.name = value
   }
 
   updatePassword(value: string): void {
+    UserEntity.validate({
+        ...this.props,
+        password: value,
+      })
     this.password = value
   }
 
@@ -40,5 +50,11 @@ export class UserEntity extends Entity<UserProps> {
 
   get createdAt() {
     return this.props.createdAt
+  }
+
+
+  static validate(props: UserProps) {
+    const validator = UserValidatorFactory.create()
+    validator.validate(props)
   }
 }
