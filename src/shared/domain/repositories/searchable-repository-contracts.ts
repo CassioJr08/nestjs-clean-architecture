71b25d8138f11a04caa0ff04f9873033e9859089
Cassio <cassiojr0108@gmail.com>
2024-02-07 21:23:11 -0300

    Adding applySort method in InMemorySearchableRepository class

--- src/shared/domain/repositories/searchable-repository-contracts.ts
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -133,5 +133,6 @@ export class SearchResult<E extends Entity, Filter = string> {
   SearchInput = SearchParams,
   SearchOutput = SearchResult<E, Filter>,
 > extends RepositoryInterface<E> {
+    sortableFields: string[]
   search(props: SearchInput): Promise<SearchOutput>
 }
