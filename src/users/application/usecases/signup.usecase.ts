641ff64ba00a80472d794877067640dc8139f1ad
Cassio <cassiojr0108@gmail.com>
2024-02-11 20:18:21 -0300

    save signup

--- src/users/application/usecases/signup.usecase.ts
+++ src/users/application/usecases/signup.usecase.ts
@@ -1,7 +1,7 @@
 import { UserRepository } from "@/users/domain/repositories/user.repository"
 import { BadRequestError } from "../../../shared/application/errors/bad-request-error"
 import { UserEntity } from "@/users/domain/entities/user.entity"
-import { HashProvider } from "@/shared/application/providers/hash-provider"
+import { HashProvider } from "@/users/infrastructure/providers/hash-provider"
 import { UserOutput, UserOutputMapper } from "../dtos/user-output"
 import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case'
 
