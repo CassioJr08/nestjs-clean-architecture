8a2bfbdcf516587bd1da72801caa15acc2e611b1
Cassio <cassiojr0108@gmail.com>
2024-02-19 14:18:37 -0300

    Adding WrapperDataInterceptor - part 1

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
-import { NestFactory, Reflector } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core'
+import { AppModule } from './app.module'
 import {
-    FastifyAdapter,
-    NestFastifyApplication,
-} from '@nestjs/platform-fastify';
-import { AppModule } from './app.module';
-import { ClassSerializerInterceptor } from '@nestjs/common';
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify'
+import { ClassSerializerInterceptor } from '@nestjs/common'
+import { applyGlobalConfig } from './global-config'
 
 async function bootstrap() {
-    const app = await NestFactory.create<NestFastifyApplication>(
-        AppModule,
-        new FastifyAdapter(),
-    );
-    app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
-    await app.listen(3000, '0.0.0.0')
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  )
+  applyGlobalConfig(app)
+  await app.listen(3000, '0.0.0.0')
 }
-bootstrap();
+bootstrap()
