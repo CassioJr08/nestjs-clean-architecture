7c7b24edf9dec1ac03f9dc36c722a36b77b9bdfe
Cassio <cassiojr0108@gmail.com>
2024-02-20 11:06:29 -0300

    Refactoring EnvConfigProvider including JWT features

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -11,4 +11,11 @@ export class EnvConfigService implements EnvConfig {
     getNodeEnv(): string {
         return this.confiservice.get<string>('NODE_ENV');
     }
+    getJwtSecret(): string {
+        return this.confiservice.get<string>('JWT_SECRET')
+    }
+
+      getJwtExpiresInSeconds(): number {
+        return Number(this.confiservice.get<number>('JWT_EXPIRES_IN'))
+    }
 }
