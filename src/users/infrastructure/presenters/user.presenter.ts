8b1e7d075c0f7463479c6f1bab6243400b770ac7
Cassio <cassiojr0108@gmail.com>
2024-02-24 11:44:47 -0300

    Refactoring the UserPresenter class with the documentation

--- src/users/infrastructure/presenters/user.presenter.ts
+++ src/users/infrastructure/presenters/user.presenter.ts
@@ -2,11 +2,19 @@ import { UserOutput } from '@/users/application/dtos/user-output'
 import { ListUsersUseCase } from '@/users/application/usecases/listusers.usecase'
 import { Transform } from 'class-transformer'
 import { CollectionPresenter } from './collection.presenter'
+import { ApiProperty } from '@nestjs/swagger'
 
 export class UserPresenter {
-  id: string
-  name: string
-  email: string
+    @ApiProperty({ description: 'Identificação do usuário' })
+    id: string
+
+    @ApiProperty({ description: 'Nome do usuário' })
+    name: string
+
+    @ApiProperty({ description: 'E-mail do usuário' })
+    email: string
+
+  @ApiProperty({ description: 'Data de criação do usuário' })
   @Transform(({ value }: { value: Date }) => value.toISOString())
   createdAt: Date
 
